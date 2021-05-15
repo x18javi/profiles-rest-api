1ab9a1e230b10ed9fbfea5a2b76181023b822415
Javi T <tjtucker92@hotmail.co.uk>
2021-05-15 12:56:55 +0100

    Picking back up after a week, saving progress

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -20,14 +20,6 @@ class UserProfileSerializer(serializers.ModelSerializer):
             }
         }
 
-    def update(self, instance, validated_data):
-        """Handle updating user account"""
-        if 'password' in validated_data:
-            password = validated_data.pop('password')
-            instance.set_password(password)
-
-        return super().update(instance, validated_data)
-
     def create(self, validated_data):
         """Create and return a new user"""
         user = models.UserProfile.objects.create_user(
@@ -37,3 +29,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
